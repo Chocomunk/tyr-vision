95904da403b825fa9af03cd828f762774b3b00c9
William Meng <wlmeng11@gmail.com>
2016-01-21 18:53:54 -0800

    Edge detect in purple as an overlay over the original video

--- vision.py
+++ vision.py
@@ -1,4 +1,8 @@
 #!/usr/bin/env python2
+#
+# tyr-vision
+# Team 8 Stronghold vision program
+#
 
 import numpy as np
 import cv2
@@ -9,8 +13,14 @@ cap = cv2.VideoCapture('flashlight-tylersjacket.mp4')  # video stream from an MP
 
 while(cap.isOpened()):
     ret, frame = cap.read()  # read a frame
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)  # convert the image to greyscale
-    cv2.imshow('frame', gray)  # show the image output on-screen
+    #gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)  # convert the image to greyscale
+    edges = cv2.Canny(frame, 100, 200)
+    color_edges = cv2.cvtColor(edges, cv2.COLOR_GRAY2BGR)
+    #color_edges[:,:,0] = 0  # set Blue channel to zero
+    color_edges[:,:,1] = 0  # set Green channel to zero
+
+    overlay_img = cv2.addWeighted(frame, 0.7, color_edges, 0.3, 0)
+    cv2.imshow('tyr-vision', overlay_img)  # show the image output on-screen
 
     if cv2.waitKey(1) & 0xFF == ord('q'):  # exit with the 'q' key
         break
