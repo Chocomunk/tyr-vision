df756ddf7d5ae58218e511e010a1e1e1d5a5ba2b
Tyler Packard <tyler.packard86@gmail.com>
2016-01-22 16:47:39 -0800

    Rectangle detection

--- tyr-vision.py
+++ tyr-vision.py
@@ -18,10 +18,18 @@ while(cap.isOpened()):
         #gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)  # convert the image to greyscale
 
         white = cv2.inRange(frame, (245, 245, 245), (255, 255, 255))
-        
-        cv2.imshow('tyr-vision', white)  # show the image output on-screen
+        contours, hierarchy = cv2.findContours(white, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
 
-        if cv2.waitKey(1) & 0xFF == ord('q'):  # exit with the 'q' key
+        img = np.zeros((720, 1280, 3), np.uint8)
+        for contour in contours:
+            approx = cv2.approxPolyDP(contour, 0.1 * cv2.arcLength(contour, True), True)
+            area = cv2.contourArea(approx)
+            if area > 4000 and len(approx) == 4:
+                cv2.drawContours(img, [approx], 0, (0, 0, 255), 5)
+        
+        cv2.imshow('tyr-vision', img)  # show the image output on-screen
+
+        if cv2.waitKey(25) & 0xFF == ord('q'):  # exit with the 'q' key
             break
     else:
         break
