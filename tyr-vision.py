8044a8fba0e4afe237fb3c9cb92568c1fa1a8be3
Tyler Packard <tyler.packard86@gmail.com>
2016-01-27 16:43:47 -0800

    Better shape detection with matchShapes

--- tyr-vision.py
+++ tyr-vision.py
@@ -13,6 +13,11 @@ import cv2
 #cap = cv2.VideoCapture('close-up-mini-U.mp4')  # https://goo.gl/photos/ECz2rhyqocxpJYQx9
 cap = cv2.VideoCapture('mini-field.mp4')  # https://goo.gl/photos/ZD4pditqMNt9r3Vr6
 
+# Load expected U shape and extract contour
+goal_img = cv2.imread('goal.png', 0)
+goal_contours, hierarchy = cv2.findContours(goal_img, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
+goal_contour = goal_contours[0]
+
 while(cap.isOpened()):
     ret, frame = cap.read()  # read a frame
     if ret:
@@ -22,6 +27,8 @@ while(cap.isOpened()):
         contours, hierarchy = cv2.findContours(white, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)  # find contours in the thresholded image
 
         # Approximate outlines into polygons
+        best_match = None # Stores best matching contour for U shape
+        best_match_similarity = 1000 # Similarity of said contour to expected U shape
         for contour in contours:
             approx = cv2.approxPolyDP(contour, 0.01 * cv2.arcLength(contour, True), True)  # smoothen the contours into simpler polygons
             # Filter through contours to detect a goal
@@ -31,13 +38,15 @@ while(cap.isOpened()):
                 cv2.rectangle(frame, (x,y), (x+w,y+h), (0,255,0), 2)  # draw bounding rectangle in green
                 if 1.2 < w/h < 1.8:  # filter by aspect ratio of the bounding box
                     cv2.drawContours(frame, [approx], 0, (255, 0, 255), 5)  # draw the contour in purple
-                """
-                # find the smallest bounding rectangle with rotation
-                rect = cv2.minAreaRect(approx)
-                box = cv2.cv.BoxPoints(rect)
-                box = np.int0(box)
-                cv2.drawContours(frame, [box], 0, (255,0,0), 2)  # draw the rotated rectangle in blue
-                """
+                    similarity = cv2.matchShapes(approx, goal_contour, 3, 0)
+                    if similarity < best_match_similarity and similarity < 0.8: # Record contour most similar to U shape
+                        best_match_similarity = similarity
+                        best_match = approx
+
+        if best_match is None:
+            print "No match"
+        else:
+            cv2.drawContours(frame, [best_match], 0, (255, 255, 0), 6) # Draw best match for U shape
 
         cv2.imshow('tyr-vision', frame)  # show the image output on-screen
 
