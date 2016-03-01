f01e113102c0155463173a0d7a695128174b61d4
William Meng <wlmeng11@gmail.com>
2016-03-01 09:39:04 -0800

    Modularized networking code

--- networking.py
+++ networking.py
@@ -3,3 +3,59 @@
 # Module for networking to the Driver Station
 
 import socket
+
+
+streaming = False
+frame_until_stream = 2
+
+s = None
+IP = "10.0.8.202"
+PORT = 56541
+
+
+# TODO: make this function take IP and PORT as arguments
+def try_connection():
+    """ Try to create the socket connection. """
+    global streaming
+    global s
+
+    while 1:
+        if not streaming:
+            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            s.bind((IP, PORT))
+            s.listen(1)
+            print "Listening"
+            s = s.accept()[0]
+
+            streaming = True
+#            except:
+#            streaming = False
+
+
+def send_video(frame):
+    try:
+        data = ""  # the data we are going to send
+        counter = 120  # size of each packet
+        sent = 0  # for debugging
+        s.send(chr(0))  # Send a null byte to mark a new frame
+        for i in xrange(120):  #height
+            for j in xrange(160): #width
+                if counter == 1: # if we need to send
+
+                    sent+=1 # for debugging
+                    s.send(chr(sent)+data+chr(frame[i,j]))#send the data
+                    data = ""
+                    counter = 120
+
+                else:
+                    data+=chr(frame[i][j])
+                    counter-=1
+        # lost = s.recv(1024)
+
+        # for i in xrange(len(lost)):
+        #     num_lost = ord(lost[i])
+        #     s.send(chr(num_lost)+all_parts[num_lost])
+    except:
+        print "Error In Send Video"
+        pass
+
--- tyr-vision.py
+++ tyr-vision.py
@@ -11,7 +11,6 @@ from __future__ import division  # always use floating point division
 import sys
 import time
 import cv2
-import socket
 from threading import Thread
 
 """ LOCAL MODULE IMPORTS """
@@ -35,65 +34,7 @@ videoinput.open_stream(settings.device)
 if settings.save_video:
     videooutput.start_recording(settings.codec)
 
-
-"""
-Video Streaming
-"""
-streaming = False
-frame_until_stream = 2
-
-s = None
-IP = "10.0.8.202"
-PORT = 56541
-
-
-def try_connection():
-    """Try to create the socket connection."""
-    global streaming
-    global s
-
-    while 1:
-        if not streaming:
-            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            s.bind((IP, PORT))
-            s.listen(1)
-            print "Listening"
-            s = s.accept()[0]
-
-            streaming = True
-#            except:
-#            streaming = False
-
-
-Thread(target=try_connection).start()
-
-
-def send_video(frame):
-    try:
-        data = ""  # the data we are going to send
-        counter = 120  # size of each packet
-        sent = 0  # for debugging
-        s.send(chr(0))  # Send a null byte to mark a new frame
-        for i in xrange(120):  #height
-            for j in xrange(160): #width
-                if counter == 1: # if we need to send
-
-                    sent+=1 # for debugging
-                    s.send(chr(sent)+data+chr(frame[i,j]))#send the data
-                    data = ""
-                    counter = 120
-
-                else:
-                    data+=chr(frame[i][j])
-                    counter-=1
-        # lost = s.recv(1024)
-
-        # for i in xrange(len(lost)):
-        #     num_lost = ord(lost[i])
-        #     s.send(chr(num_lost)+all_parts[num_lost])
-    except:
-        print "Error In Send Video"
-        pass
+Thread(target=networking.try_connection).start()
 
 
 
@@ -113,10 +54,11 @@ while(videoinput.cap.isOpened()):
         videooverlay.draw_base_HUD(frame)
 
         try:
-            if streaming and frame_until_stream == 0:
-                send_video(cv2.cvtColor(cv2.resize(frame,(160,120)), cv2.COLOR_BGR2GRAY))
-                frame_until_stream = 2
-            else: frame_until_stream -=1
+            # TODO: these variables should be moved to settings.py
+            if networking.streaming and networking.frame_until_stream == 0:
+                networking.send_video(cv2.cvtColor(cv2.resize(frame,(160,120)), cv2.COLOR_BGR2GRAY))
+                networking.frame_until_stream = 2
+            else: networking.frame_until_stream -=1
         except:
             print "Error in video streaming Loop"
             pass
