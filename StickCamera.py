d98a1a435e22c9142844ecc93b8c94dab86a9005
William Meng <wlmeng11@gmail.com>
2016-03-11 13:30:48 -0800

    Updated StickCamera

--- StickCamera.py
+++ StickCamera.py
@@ -1,10 +1,35 @@
+#!/usr/bin/env python2
+#
+# StickCamera.py
+
 import cv2
 
-cap = cv2.VideoCapture(0)
+WIDTH = 640
+HEIGHT = 480
 
-cv2.namedWindow("Tyr-Vision", cv2.cv.CV_WINDOW_NORMAL)
+try:
+    cap = cv2.VideoCapture(1)
+except:
+    cap = cv2.VideoCapture(0)
+
+# set resolution to 1080p
+cap.set(cv2.cv.CV_CAP_PROP_FRAME_WIDTH, 1920)
+cap.set(cv2.cv.CV_CAP_PROP_FRAME_HEIGHT, 1080)
+
+# make the window resizable
+cv2.namedWindow("stick", cv2.cv.CV_WINDOW_NORMAL)
+cv2.resizeWindow("stick", WIDTH, HEIGHT)
+
 
 while cap.isOpened():
-	print cap.read()[0]
-	cv("Tyr-Vision", cap.read()[1])
-	cv2.waitKey(1)
+    ret, frame = cap.read()  # read a frame
+    cv2.imshow('stick', frame)
+    k = cv2.waitKey(1)
+    if k == ord('q') or k == 27:  # exit with the 'q' or 'esc' key
+        print "Exiting playback!"
+        break
+
+
+# Cleanup
+cap.release()
+cv2.destroyAllWindows()
