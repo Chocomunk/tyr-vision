13350af6a80c86b2ab3601a4176f8e90e237f057
William Meng <wlmeng11@gmail.com>
2016-03-11 17:38:27 -0800

    Video recording without dependencies

--- StickCamera.py
+++ StickCamera.py
@@ -2,6 +2,7 @@
 #
 # StickCamera.py
 
+import time
 import cv2
 
 WIDTH = 640
@@ -24,10 +25,25 @@ try:
     cv2.resizeWindow("stick", WIDTH, HEIGHT)
 except: pass
 
+video_writer = None
+try:
+    filename = time.strftime("%Y-%m-%d_%H-%M-%S.avi")
+    CODEC = 1196444237
+    FRAMERATE = 30
+    video_writer = cv2.VideoWriter(filename, CODEC, FRAMERATE, (WIDTH, HEIGHT))
+    print "Writing to video file %s" % filename
+except:
+    print "Failed to write video file!"
+
 
 while cap.isOpened():
     ret, frame = cap.read()  # read a frame
     cv2.imshow('stick', frame)
+    try:
+        video_writer.write(frame)
+    except:
+        print "Couldn't write frame!"
+
     k = cv2.waitKey(1)
     if k == ord('q') or k == 27:  # exit with the 'q' or 'esc' key
         print "Exiting playback!"
